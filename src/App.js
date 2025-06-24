a7d34fdd8434c6b50051f2668127e1369b0a1f19
Pushkin Anton <pushkinanton2002@gmail.com>
2025-06-24 20:55:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Page404 from './pages/404';
+import MainPage from './pages/main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route exact path='/' Component={MainPage}/>
+          <Route exact path='/404' Component={Page404}/>
+          <Route path='*' element={<Navigate to='/404' replace/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/main/index.js
@@ -0,0 +1,11 @@
+
+
+const MainPage = () => {
+    return (
+        <div>
+            fgfgfg
+        </div>
+    )
+}
+
+export default MainPage;
